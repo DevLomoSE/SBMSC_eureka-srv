30d6729e8f61af24954ab1009754b942c42f2148
Jonathan Guzman Guadarrama <jonathancggs@gmail.com>
2022-04-14 21:31:12 -0500

    :tada: Init
    Begin of the project

---
+++ src/main/java/com/devlomo/sbms/srv/eureka/app/MsEurekaSrvApplication.java
@@ -0,0 +1,13 @@
+package com.devlomo.sbms.srv.eureka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaSrvApplication.class, args);
+	}
+
+}
